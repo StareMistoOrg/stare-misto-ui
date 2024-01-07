dd0c861b33bf5f03c055aa18679cea067b8c1e04
Serhii Staryk <serhii.staryk@gmail.com>
2024-01-07 03:06:32 +0200

    fix code

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://StareMistoOrg.github.io/stare-misto-ui/',
+  base: 'https://staremistoorg.github.io/stare-misto-ui/',
 });
