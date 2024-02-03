7e000ff228c528a538e105d58e79979c07a1da9d
Serhii Staryk <serhii.staryk@gmail.com>
2024-02-03 22:03:52 +0200

    fix hot bug

--- src/components/Sidebar/Burger/index.tsx
+++ src/components/Sidebar/Burger/index.tsx
@@ -9,10 +9,8 @@ const Burger = () => {
     document.body.style.position = 'fixed';
     document.body.style.top = `-${window.scrollY}px`;
   } else {
-    const scrollY = document.body.style.top;
     document.body.style.position = '';
     document.body.style.top = '';
-    window.scrollTo(0, parseInt(scrollY || '0') * -1);
   }
 
   return (
