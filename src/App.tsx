b6245e56f61f7db0f7edf4ad36a6715608992428
Serhii Staryk <serhii.staryk@gmail.com>
2024-02-25 01:06:51 +0200

    fix-routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,27 @@ import Layout from './pages/Layout';
 import HomePage from './pages/Home';
 import AuthenticationPage from './pages/Authentication';
 
-const routes = [
-  {
-    path: '/stare-misto-ui/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'auth',
-        element: <AuthenticationPage />,
-      },
-    ],
-  },
-];
+const App: React.FC = () => {
+  const routes = [
+    {
+      path: '/stare-misto-ui/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: 'auth',
+          element: <AuthenticationPage />,
+        },
+      ],
+    },
+  ];
 
-const router = createBrowserRouter(routes);
+  const router = createBrowserRouter(routes, { basename: import.meta.env.BASE_URL ? '/' : '/stare-misto-ui' });
 
-const App: React.FC = () => <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
+};
 
 export default App;
